f8c0df3255c0c044285fffb05e1bd80da96905d8
abdullahsiddiqui810 <41952870+AhsaanSiddiqui@users.noreply.github.com>
2026-04-25 10:00:31 +0500

    mini context

---
+++ minicontext/src/Compouned/Login.jsx
@@ -0,0 +1,25 @@
+import React, {useState , useContext} from 'react'
+import UserContext from '../context/UserContext'
+export default function Login() {
+    const [username ,setUsername] = useState('')
+    const [password ,setPassword] = useState('')
+
+    const {setUser} = useContext(UserContext)
+    
+    const handleSubmit = (e) =>{
+      
+      setUser({username ,password})
+    }
+  return (
+    <div>
+      <h2>Login</h2>
+      <input type="text" 
+      value={username}
+      placeholder='username' 
+      onChange={(e) => setUsername(e.target.value)}/>
+
+      <input type="text" placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  )
+}
